6d63e0377af68711717d6c6ababc1347e1593d95
keros1n (GM) <marselch2017@gmail.com>
2025-01-17 18:25:34 +0400

    Добавьте файлы проекта.

---
+++ ShopOfTanks/MainForm.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ShopOfTanks
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.AusPanel = new System.Windows.Forms.Panel();
+            this.RegButton = new System.Windows.Forms.Button();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.AusButton = new System.Windows.Forms.Button();
+            this.FiltrPanel = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.CategoryComboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.HideButton = new System.Windows.Forms.Button();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AusPanel.SuspendLayout();
+            this.FiltrPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AusPanel
+            // 
+            this.AusPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.AusPanel.Controls.Add(this.RegButton);
+            this.AusPanel.Controls.Add(this.NameLabel);
+            this.AusPanel.Controls.Add(this.AusButton);
+            this.AusPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.AusPanel.Location = new System.Drawing.Point(0, 0);
+            this.AusPanel.Name = "AusPanel";
+            this.AusPanel.Size = new System.Drawing.Size(1123, 67);
+            this.AusPanel.TabIndex = 0;
+            // 
+            // RegButton
+            // 
+            this.RegButton.Location = new System.Drawing.Point(466, 9);
+            this.RegButton.Name = "RegButton";
+            this.RegButton.Size = new System.Drawing.Size(251, 40);
+            this.RegButton.TabIndex = 2;
+            this.RegButton.Text = "Зарегистрироваться";
+            this.RegButton.UseVisualStyleBackColor = true;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(193, 17);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(64, 25);
+            this.NameLabel.TabIndex = 1;
+            this.NameLabel.Text = "label1";
+            // 
+            // AusButton
+            // 
+            this.AusButton.Location = new System.Drawing.Point(12, 9);
+            this.AusButton.Name = "AusButton";
+            this.AusButton.Size = new System.Drawing.Size(150, 40);
+            this.AusButton.TabIndex = 0;
+            this.AusButton.Text = "ВОЙТИ";
+            this.AusButton.UseVisualStyleBackColor = true;
+            // 
+            // FiltrPanel
+            // 
+            this.FiltrPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.FiltrPanel.Controls.Add(this.label2);
+            this.FiltrPanel.Controls.Add(this.SearchButton);
+            this.FiltrPanel.Controls.Add(this.CategoryComboBox1);
+            this.FiltrPanel.Controls.Add(this.label1);
+            this.FiltrPanel.Controls.Add(this.NameTextBox);
+            this.FiltrPanel.Controls.Add(this.HideButton);
+            this.FiltrPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.FiltrPanel.Location = new System.Drawing.Point(0, 67);
+            this.FiltrPanel.Name = "FiltrPanel";
+            this.FiltrPanel.Size = new System.Drawing.Size(1123, 131);
+            this.FiltrPanel.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(418, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Категория";
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(954, 57);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(108, 36);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "найти";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // CategoryComboBox1
+            // 
+            this.CategoryComboBox1.FormattingEnabled = true;
+            this.CategoryComboBox1.Location = new System.Drawing.Point(548, 57);
+            this.CategoryComboBox1.Name = "CategoryComboBox1";
+            this.CategoryComboBox1.Size = new System.Drawing.Size(220, 33);
+            this.CategoryComboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Название";
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(170, 60);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(220, 30);
+            this.NameTextBox.TabIndex = 1;
+            // 
+            // HideButton
+            // 
+            this.HideButton.Location = new System.Drawing.Point(12, 17);
+            this.HideButton.Name = "HideButton";
+            this.HideButton.Size = new System.Drawing.Size(150, 40);
+            this.HideButton.TabIndex = 0;
+            this.HideButton.Text = "скрыть";
+            this.HideButton.UseVisualStyleBackColor = true;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.Blue;
+            this.MainPanel.Controls.Add(this.label4);
+            this.MainPanel.Controls.Add(this.pictureBox2);
+            this.MainPanel.Controls.Add(this.label3);
+            this.MainPanel.Controls.Add(this.pictureBox1);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 198);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(1123, 367);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(65, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Т-34";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(236, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(171, 166);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(266, 189);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 25);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Т34(1776)";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 565);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.FiltrPanel);
+            this.Controls.Add(this.AusPanel);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.AusPanel.ResumeLayout(false);
+            this.AusPanel.PerformLayout();
+            this.FiltrPanel.ResumeLayout(false);
+            this.FiltrPanel.PerformLayout();
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel AusPanel;
+        private System.Windows.Forms.Panel FiltrPanel;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.Button RegButton;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.Button AusButton;
+        private System.Windows.Forms.Button HideButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ComboBox CategoryComboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
